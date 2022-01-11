5a05d05f8f25c38c4435dc8d6bf9c5bd3c00a449
Peter Kriens <Peter.Kriens@aqute.biz>
2022-01-11 19:25:36 +0100

    [warning] Cleaned up warning
    Signed-off-by: Peter Kriens <Peter.Kriens@aqute.biz>

--- org.paremus.netty.tls/test/com/paremus/netty/test/AbstractDTLSTest.java
+++ org.paremus.netty.tls/test/com/paremus/netty/test/AbstractDTLSTest.java
@@ -20,7 +20,6 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.Security;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.util.concurrent.BlockingQueue;
